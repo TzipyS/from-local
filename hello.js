2761a9706a9bfd13fb34be94c9af5567025a65cb
TzipyS <t633042gmail.com>
2025-12-10 02:57:57 +0200

    add e function

--- hello.js
+++ hello.js
@@ -5,3 +5,11 @@ const b = () => {
     alert("main")
     return 5
 }
+
+const e = () => {
+    arr =[1,2,3,4,5]
+    const result =(num)=>{
+        arr.find(num>4)
+    }
+    console.log(result)
+}
